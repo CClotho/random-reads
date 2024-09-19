924f3fd2c4effe123e15d5a9fdd12d3652220947
CClotho <124051470+CClotho@users.noreply.github.com>
2024-09-19 11:01:58 +0800

    (added): absolute url path for files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+/*https://stackoverflow.com/questions/68241263/absolute-path-not-working-in-vite-project-react-ts */
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+  
 })
+
+
+/**
+ * "paths": {
+      "@assets/*": ["./assets/*"],
+      "@components/*": ["./components/*"],
+      "@config/*": ["./config/*"],
+      "@hooks/*": ["./hooks/*"],
+      "@ioc/*": ["./ioc/*"],
+      "@pages/*": ["./pages/*"],
+      "@utils/*": ["./utils/*"]
+    },
+ */

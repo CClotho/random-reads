56d56c2fb62e58809d89b7de4b9f601ca1d626dd
CClotho <124051470+CClotho@users.noreply.github.com>
2024-09-27 10:58:31 +0800

    (edited)content

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 
-import './App.css'
+import '@styles/global/app.scss';
+import 'App.css';
 import { Link, Outlet } from 'react-router-dom';
 import LoginForm from '@components/login-feature/LoginForm';
 import SignUpForm from '@components/signup-feature/SignUpForm';
-import Avatar from '@components/common/Avatar';
+import NavBar from '@components/layout/NavBar';
+
 
 function App() {
 
 
   return (
     
-    <>
+    <main>
      {/* Not working*/}
-     <div>
-     <Link to="/story"> Go to story page</Link>
-     <Link to="/"> Go to new</Link>
-     </div>
-      <SignUpForm/>
-      <LoginForm/>
+      <NavBar/>
+      <br/>
       <Outlet />
-    </>
+     {/*  <SignUpForm/>
+      <br/>
+      <LoginForm/> */}
+    
+    </main>
   )
 }
 
